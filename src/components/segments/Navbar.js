11325bd9b88094f90727875bc1101fae201cdfc3
Matus Kempa <matus.kempa@gmail.com>
2021-03-23 15:47:25 +0100

    Header banner

--- src/components/segments/Navbar.js
+++ src/components/segments/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
-  Nav, Navbar, NavItem,
+  Col,
+  Grid,
+  Nav, Navbar, NavItem, Row,
 } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -8,35 +10,42 @@ import { LinkContainer } from 'react-router-bootstrap';
 import config from '../../config';
 
 const CNavbar = () => (
-  <div id="navigation">
-    <Navbar collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <Link to={config.routes.home}>Cardamine</Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          <LinkContainer exact to={config.routes.home}>
-            <NavItem>
-              Home
-            </NavItem>
-          </LinkContainer>
-          <LinkContainer exact to={config.routes.checklist}>
-            <NavItem>
-              Names
-            </NavItem>
-          </LinkContainer>
-          <LinkContainer exact to={config.routes.chromosomes}>
-            <NavItem>
-              Chromosome and ploidy level data
-            </NavItem>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
+  <>
+    <Grid>
+      <Row>
+        <Col id="header-banner" xsHidden />
+      </Row>
+    </Grid>
+    <div id="navigation">
+      <Navbar collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to={config.routes.home}>CardaBase</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <LinkContainer exact to={config.routes.home}>
+              <NavItem>
+                Home
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer exact to={config.routes.checklist}>
+              <NavItem>
+                Names
+              </NavItem>
+            </LinkContainer>
+            <LinkContainer exact to={config.routes.chromosomes}>
+              <NavItem>
+                Chromosome and ploidy level data
+              </NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  </>
 );
 
 export default CNavbar;
