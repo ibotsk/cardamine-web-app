2e9672e9855b3bded1a91d34e2d92ecb8c15a078
Matus Kempa <matus.kempa@gmail.com>
2021-03-24 10:23:28 +0100

    Updated text on homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,39 +5,44 @@ const Home = () => (
   <Grid className="text-justify">
     <Row>
       <Col>
-        <h3>Database</h3>
+        <h3>
+          CardaBase
+          {' '}
+          – Datababase of names, chromosome numbers, ploidy levels
+          and genome sizes of the tribe Cardamineae
+        </h3>
         <p>
-          CardamBase – Datababase of names, chromosome numbers and ploidy levels
-          of the genus Cardamine
-        </p>
-        <p>
-          Database of names and published chromosome numbers and ploidy-level
-          estimates of the genus Cardamine (to be later extended to the whole
-          tribe Cardamineae) is presented.  The database comprises the list of
+          Database of names and published chromosome numbers, ploidy-level
+          estimates and genome sizes of the tribe Cardamineae is established
+          here. Currently the database comprises only data on the largest genus
+          of the tribe, namely Cardamine. Data on the other genera of the tribe
+          will be continuously added. The database comprises the list of
           accepted names and their synonyms (including places of publication,
           type localities, and type specimens, wherever information is
           available) reflecting the most recent taxonomic and phylogenetic
-          studies in this genus.
+          studies. Currently the database includes almost 300 accepted names
+          of species and infraspecific taxa, altogether nearly thousand names
+          of taxa, including synonyms. It also includes data on 3701 chromosome
+          number records, 750 DNA ploidy level records and 150 genome size
+          records.
         </p>
         <p>
-          Currently the database includes almost 300 accepted names of species
-          and infraspecific taxa, altogether 946 names of taxa, including
-          synonyms.
-        </p>
-        <p>
-          It also includes data on 3701 chromosome number records, 750 DNA
-          ploidy level records and 150 genome size records.
+          Currently the database includes almost 300 accepted names
+          of species and infraspecific taxa, altogether nearly thousand names
+          of taxa, including synonyms. It also includes data on 3701 chromosome
+          number records, 750 DNA ploidy level records and 150 genome size
+          records.
         </p>
       </Col>
     </Row>
     <Row>
       <Col>
-        <h3>Recommended citation</h3>
-        <p>
+        <h4>Recommended citation</h4>
+        <p style={{ fontSize: 'smaller' }}>
           Karol Marhold, Matúš Kempa, Jaromír Kučera, Katarína Skokanová, Janka
-          Smatanová, Barbora Šingliarová, Marek Šlenker, Judita Lihová-Zozomová
-          (2021+): CardamBase – Datababase of names, chromosome numbers and
-          ploidy levels of the genus Cardamine. Available at:
+          Smatanová, Barbora Šingliarová, Marek Šlenker, Judita Zozomová-Lihová
+          (2021+): Datababase of names, chromosome numbers, ploidy levels and
+          genome sizes of the tribe Cardamineae. Available at:
           https://cardamine.sav.sk
         </p>
       </Col>
